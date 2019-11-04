c4a4b5ca7bfd31abbc9475e8c1de1ad0c0b0dec0
Patrick O'Brien <patrick@patrickmobrien.com>
2019-11-04 07:48:43 -0500

    Assign colors with miro

---
+++ app/interactors/set_team_color.rb
@@ -0,0 +1,12 @@
+class SetTeamColor
+  include Interactor
+
+  def call
+    team = context.team
+    colors = Miro::DominantColors.new(team.logo_url)
+    color = colors.to_hex.first
+    unless team.update(color: color)
+      context.fail!(message: "Something went wrong while assigning the color")
+    end
+  end
+end
