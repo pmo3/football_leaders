68f3bd34c76deb390672f894394c6f6814225f88
Patrick O'Brien <patrick@patrickmobrien.com>
2019-11-04 08:26:29 -0500

    don't update colors if already existing

--- app/interactors/set_team_color.rb
+++ app/interactors/set_team_color.rb
@@ -3,10 +3,12 @@ class SetTeamColor
 
   def call
     team = context.team
-    colors = Miro::DominantColors.new(team.logo_url)
-    color = colors.to_hex.first
-    unless team.update(color: color)
-      context.fail!(message: "Something went wrong while assigning the color")
+    unless team.color.present?
+      colors = Miro::DominantColors.new(team.logo_url)
+      color = colors.to_hex.first
+      unless team.update(color: color)
+        context.fail!(message: "Something went wrong while assigning the color")
+      end
     end
   end
 end
